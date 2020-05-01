a4ca58a45ddf9f06d88cc4bb1d87dd5afd1747c5
weis95 <41548530+weis95@users.noreply.github.com>
2020-05-01 16:59:42 +0200

    asd

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS student (
+    task_id int(5) NOT NULL AUTO_INCREMENT,
+    task_name varchar(50) DEFAULT NULL,
+    task_status varchar(50) DEFAULT NULL,
+    PRIMARY KEY(task_id)
+    );
